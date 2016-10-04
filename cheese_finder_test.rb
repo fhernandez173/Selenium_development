58331401b128104e60e73b44b631e87755969e24
fhernanez173 <f.hernandez173@gmail.com>
2016-10-04 17:08:03 -0400

    Used assertion

--- cheese_finder_test.rb
+++ cheese_finder_test.rb
@@ -12,6 +12,11 @@ class CheeseFinderTests < Test::Unit::TestCase
     driver.find_element(:id, "searchinput").send_keys("cheese")
     driver.find_element(:id, "searchsubmit").click
 
+    # Use assert to find out if something is true
+    assert(driver.find_element(:class, "entry").text.include?("No Results Found"))
+    # following fails
+    # assert(driver.find_element(:class, "entry").text.include?("5 Results Found"))
+
     driver.quit
   end
 
