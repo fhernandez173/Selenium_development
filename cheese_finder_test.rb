edd1e0e72c178577cb47a31ffe78f8a66fe9dda4
fhernanez173 <f.hernandez173@gmail.com>
2016-10-04 17:14:46 -0400

    Learned to use debug, could use pry instead

--- cheese_finder_test.rb
+++ cheese_finder_test.rb
@@ -7,6 +7,7 @@ class CheeseFinderTests < Test::Unit::TestCase
   # In Test:Unit framework, all test methods must begin with test_ or they will be ignored
   def test_find_some_cheese
     driver = Selenium::WebDriver.for(:firefox)
+    # require 'debug' # can be used step by step to see where error occured
     driver.get("http://awful-valentine.com/")
     driver.find_element(:id, "searchinput").clear
     driver.find_element(:id, "searchinput").send_keys("cheese")
