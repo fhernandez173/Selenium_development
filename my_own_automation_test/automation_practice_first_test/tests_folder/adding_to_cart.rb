f6007e09656aa0b69f868c4e57dd031d9ae86621
fhernanez173 <f.hernandez173@gmail.com>
2016-11-14 16:34:59 -0500

    updated tests with data file and support methods

--- my_own_automation_test/automation_practice_first_test/tests_folder/adding_to_cart.rb
+++ my_own_automation_test/automation_practice_first_test/tests_folder/adding_to_cart.rb
@@ -1,27 +1,24 @@
 require 'rubygems'
 require 'selenium-webdriver'
+require 'test/unit'
+require_relative '../helpers/support_methods'
 
-class TestingCartAndPayment
+class TestingCartAndPayment < Test::Unit::TestCase
 
-  def test_payment_info_into_fields
-    fox_driver = Selenium::WebDriver.for(:firefox)
-    fox_driver.get("http://automationpractice.com")
+  def setup
+    @fox_driver_instance = SupportMethods.new
+  end
 
-    # Find search bar and use .clear
-    fox_driver.find_element(:id, "search_query_top").clear
-    # input something to search
-    fox_driver.find_element(:id, "search_query_top").send_keys("shirts")
-    fox_driver.find_element(:name, "submit_search").click
-    # find a certain item, and click on it, confirm I clicked on something
-    # issue below selecting what I want
-    fox_driver.find_element(:css, "a.product-name").click
-    # assertion to confirm I am in the right page
-    # add to cart
-    fox_driver.find_element(:id, "add_to_cart").find_element(:class, "exclusive").click
+  def teardown
+    @fox_driver_instance.quit
+  end
 
-    # ___________________FINISH CART TESTS____________________ #
+  def test_add_items_to_cart
+    @fox_driver_instance.get_website_url
+    assert_equal("http://automationpractice.com/index.php", @fox_driver_instance.current_url)
 
-    
+    @fox_driver_instance.add_to_cart("shirts")
+
   end
 
 end
