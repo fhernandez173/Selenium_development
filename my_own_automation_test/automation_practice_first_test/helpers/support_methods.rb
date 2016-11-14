bbdc1e92d06eee72a22cb5c44c6e52189f4d34c6
fhernanez173 <f.hernandez173@gmail.com>
2016-11-14 16:38:07 -0500

    Updated SupportMethods class

--- my_own_automation_test/automation_practice_first_test/helpers/support_methods.rb
+++ my_own_automation_test/automation_practice_first_test/helpers/support_methods.rb
@@ -33,6 +33,34 @@ class SupportMethods
 
   end
 
+  def add_to_cart(searched_item)
+    @fox_driver.find_element(:id, "search_query_top").clear
+    # input something to search
+    @fox_driver.find_element(:id, "search_query_top").send_keys(searched_item)
+    @fox_driver.find_element(:name, "submit_search").click
+    # find a certain item, and click on it, confirm I clicked on something
+    # issue below selecting what I want
+    @fox_driver.find_element(:css, "a.product-name").click
+    # assertion to confirm I am in the right page
+    # add to cart
+    @fox_driver.find_element(:id, "add_to_cart").find_element(:class, "exclusive").click
+  end
+
+  def self.searching
+    # Find search bar and use .clear
+    @fox_driver.find_element(:id, "search_query_top").clear
+    # input something to search
+    @fox_driver.find_element(:id, "search_query_top").send_keys("shirts")
+    @fox_driver.find_element(:name, "submit_search").click
+    # find a certain item, and click on it, confirm I clicked on something
+    # issue below selecting what I want
+
+    # Below create condition based if TestingData.search_term if false or true
+    @fox_driver.find_element(:css, "a.product-name").click
+  end
+
+  # _______________________ General Methods _____________________ #
+
   def get_website_url
     @fox_driver.get(TestingData.site_url)
   end
@@ -51,6 +79,10 @@ class SupportMethods
 
   def find_element(strategy, element)
     @fox_driver.find_element(strategy, element)
+  end
+
+  def clear_search_box
+    @fox_driver.clear
   end
 
 end
