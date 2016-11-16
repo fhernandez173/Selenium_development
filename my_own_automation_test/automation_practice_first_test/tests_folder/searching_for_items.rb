107cc154c0c930272a8fc5e41965741210070826
fhernanez173 <f.hernandez173@gmail.com>
2016-11-16 14:14:06 -0500

    need to sort out second test to handle no results

--- my_own_automation_test/automation_practice_first_test/tests_folder/searching_for_items.rb
+++ my_own_automation_test/automation_practice_first_test/tests_folder/searching_for_items.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'selenium-webdriver'
 require 'test/unit'
 require_relative '../helpers/support_methods'
+require 'pry'
 
 class SearchingTests < Test::Unit::TestCase
 
@@ -16,36 +17,30 @@ class SearchingTests < Test::Unit::TestCase
   def test_searching_for_item
     @fox_driver_instance.get_website_url
 
+    @fox_driver_instance.searching(true) # this works
+    binding.pry
+
+    # confirm that the page we have has results
+    product_list = @fox_driver_instance.find_element(:class, "product_list").find_element(:class, "ajax_block_product")
+
+    # Create assertions to know whether an item showed up here
+    product_count = @fox_driver_instance.find_element(:class, "product-count")
+
     # Find search bar and use .clear
-    @fox_driver_instance.find_element(:id, "search_query_top").clear
-    # input something to search
-    @fox_driver_instance.find_element(:id, "search_query_top").send_keys("shirts")
-    @fox_driver_instance.find_element(:name, "submit_search").click
-    # find a certain item, and click on it, confirm I clicked on something
-    # issue below selecting what I want
-    @fox_driver_instance.find_element(:css, "a.product-name").click
-    # assertion to confirm I am in the right page
-    # add to cart method used here, may be unnecessary
-    @fox_driver_instance.find_element(:id, "add_to_cart").find_element(:class, "exclusive").click
+    # @fox_driver_instance.add_to_cart(search_term)
 
   end
 
   def test_no_search_results
-    @search_term = "dbhwrthwthn"
     @fox_driver_instance.get_website_url
 
-    @fox_driver_instance.find_element(:id, "search_query_top").clear
-
-    @fox_driver_instance.find_element(:id, "search_query_top").send_keys(@search_term)
-    @fox_driver_instance.find_element(:name, "submit_search").click
-
-
-    @fox_driver_instance.find_element(:css, "a.product-name").click
+    @fox_driver_instance.searching(false)
 
     # add to cart
     # @fox_driver_instance.find_element(:id, "add_to_cart").find_element(:class, "exclusive").click
+    # binding.pry
     no_search_result = @fox_driver_instance.find_element(:id, "center_column").find_element(:class, "alert").text
-    assert_equal("No results were found for your search #{@search_term}", no_search_result)
+    assert_equal("No results were found for your search #{search_term}", no_search_result)
 
   end
 
